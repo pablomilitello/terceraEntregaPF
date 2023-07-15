168f8d2ce0f0ad6f4b7c516ac027ccea3662ab67
“Pablo <pmilitelloaveni@gmail.com>
2023-07-15 11:12:27 -0300

    instalo mocha y chai

--- src/swaggerSpecs.js
+++ src/swaggerSpecs.js
@@ -5,7 +5,7 @@ const swaggerOptions = {
   definition: {
     openapi: '3.0.1',
     info: {
-      title: 'Adoptme API Documentation',
+      title: 'API Documentation',
       description:
         'This documentation will help you get familiar with the resources and show you how to make different queries',
       version: '1.0.0',
